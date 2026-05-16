f0dff651341f46f0766f875d01138e7c181d0ff5
Thomas Johannesen <tgj@outlook.dk>
2026-05-16 18:06:02 +0200

    Version 1.7.4
    Updated update.js with new delpoy code

--- backend/src/routes/update.js
+++ backend/src/routes/update.js
@@ -60,7 +60,7 @@ router.get('/check', admin, async (req, res) => {
     if (!current) {
       return res.json({
         hasUpdate: false,
-        error: 'Kan ikke finde git commit — er appen installeret via git?',
+        error: 'Kan ikke finde git commit — er appen installeret via git? Prøv igen',
       });
     }
 
